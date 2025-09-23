9cf41bc28bee83205ed106899b39cbdb1c91ce7f
FaizanJavaid <bhat28430@gmail.com>
2025-09-23 22:28:56 +0530

    updated buttons

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // app.js
-
 console.log("signUp button updated!");
 console.log("signIn button updated!");
 console.log("login button updated!");
-
-
+console.log("logout button updated!");
+console.log("profile button updated!");
+console.log("settings button updated!");
+console.log("dashboard button updated!");
